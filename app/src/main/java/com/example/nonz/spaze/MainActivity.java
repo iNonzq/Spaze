1cc38bf57691073ccec77ac2cc2aaf96b790a546
Nonz <Nonz@Apples-MacBook-Pro.local>
2018-04-11 23:24:16 +0700

    add button

--- app/src/main/java/com/example/nonz/spaze/MainActivity.java
+++ app/src/main/java/com/example/nonz/spaze/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.nonz.spaze;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton a1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        a1 = (ImageButton) findViewById(R.id.imageButton2);
+        a1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent A1 = new Intent(MainActivity.this,PageCourse.class);
+                startActivity(A1);
+            }
+        });
     }
 }
-//212212
-//kkjl,l
