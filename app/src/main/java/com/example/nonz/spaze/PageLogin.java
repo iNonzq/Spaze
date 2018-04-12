12ab3260d1962e85055462d6db6500f21f7431f4
Supanut <supanut.kon@gmail.com>
2018-04-12 19:32:24 +0700

    Login Page add

---
+++ app/src/main/java/com/example/nonz/spaze/PageLogin.java
@@ -0,0 +1,19 @@
+package com.example.nonz.spaze;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cv on 4/12/2018.
+ */
+
+public class PageLogin  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_login);
+
+
+    }
+}
