25e2e1cb8b8ea38c1a8de21828c4fccffa7245b2
Nonz <Nonz@Apples-MacBook-Pro.local>
2018-04-18 23:20:16 +0700

    chang purplebg color

--- app/src/main/java/com/example/nonz/spaze/PagePlanet.java
+++ app/src/main/java/com/example/nonz/spaze/PagePlanet.java
@@ -13,21 +13,34 @@ import android.widget.TextView;
 
 public class PagePlanet extends AppCompatActivity {
     Dialog Sun_dia;
-    ImageButton s1;
+    ImageButton s1,c1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_planet);
 
         s1 = (ImageButton) findViewById(R.id.SunBtn);
+
         s1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ShowPopup();
+                //ShowPopup();
+                Sun_dia = new Dialog(PagePlanet.this);
+                Sun_dia.setContentView(R.layout.sun_dialog);
+                c1 = (ImageButton) Sun_dia.findViewById(R.id.closebtn);
+                c1.setEnabled(true);
+                c1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Sun_dia.dismiss();
+                    }
+                });
+                Sun_dia.show();
+
             }
         });
     }
-    public void ShowPopup() {
+    /*public void ShowPopup() {
         ImageButton c1;
         Sun_dia = new Dialog(PagePlanet.this);
         Sun_dia.setContentView(R.layout.sun_dialog);
@@ -40,6 +53,7 @@ public class PagePlanet extends AppCompatActivity {
                 Sun_dia.dismiss();
             }
         });
+
         Sun_dia.show();
-    }
+    }*/
 }
