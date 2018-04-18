64d22050a79b7c9b42a23d4a5ebbfec724143375
Nonz <Nonz@Apples-MacBook-Pro.local>
2018-04-18 21:36:30 +0700

    planet dialog page success 1

--- app/src/main/java/com/example/nonz/spaze/PagePlanet.java
+++ app/src/main/java/com/example/nonz/spaze/PagePlanet.java
@@ -13,17 +13,19 @@ import android.widget.TextView;
 
 public class PagePlanet extends AppCompatActivity {
     Dialog Sun_dia;
-    ImageButton SunBtn,closeBtn;
+    ImageButton s1,c1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_planet);
 
-        SunBtn = (ImageButton) findViewById(R.id.SunBtn);
-        SunBtn.setOnClickListener(new View.OnClickListener() {
+        s1 = (ImageButton) findViewById(R.id.SunBtn);
+        s1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ShowPopup();
+
+
             }
         });
     }
@@ -31,15 +33,15 @@ public class PagePlanet extends AppCompatActivity {
         Sun_dia = new Dialog(PagePlanet.this);
         Sun_dia.setContentView(R.layout.sun_dialog);
 
-        closeBtn = (ImageButton) findViewById(R.id.closebtn);
-        closeBtn.setEnabled(true);
-        closeBtn.setOnClickListener(new View.OnClickListener() {
+        /*c1 = (ImageButton) findViewById(R.id.closebtn);
+        c1.setEnabled(true);
+        c1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Sun_dia.cancel();
+                Sun_dia.dismiss();
             }
-        });
-        Sun_dia.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }); */
+
         Sun_dia.show();
     }
 }
