ef96aefbd1efe47a395e1c0c0554fd8ac1049b66
nawabhorn <38258798+nawabhorn@users.noreply.github.com>
2018-04-21 16:58:36 +0700

    DDL

--- app/src/main/java/com/example/nonz/spaze/PageCourse.java
+++ app/src/main/java/com/example/nonz/spaze/PageCourse.java
@@ -1,47 +1,22 @@
 package com.example.nonz.spaze;
 
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class PageCourse extends Activity implements AdapterView.OnCreateContextMenuListener {
-
-    private Spinner spinner;
-    private static final String[]paths = {"item 1", "item 2", "item 3"};
+public class PageCourse extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_course);
 
-        spinner = (Spinner)findViewById(R.id.spinner1);
-        ArrayAdapter<String>adapter = new ArrayAdapter<String>(PageCourse.this,
-                android.R.layout.simple_spinner_item,paths);
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
-
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner1);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(PageCourse.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
     }
-
-    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
-
-        switch (position) {
-            case 0:
-                // Whatever you want to happen when the first item gets selected
-                break;
-            case 1:
-                // Whatever you want to happen when the second item gets selected
-                break;
-            case 2:
-                // Whatever you want to happen when the thrid item gets selected
-                break;
-
-        }
-    }
-
 }
