b5e9735c5b3d65b9c4bbfc942db710c0be9cc521
nawabhorn <38258798+nawabhorn@users.noreply.github.com>
2018-04-21 16:08:23 +0700

    Dropdown list

--- app/src/main/java/com/example/nonz/spaze/PageCourse.java
+++ app/src/main/java/com/example/nonz/spaze/PageCourse.java
@@ -1,13 +1,47 @@
 package com.example.nonz.spaze;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class PageCourse extends AppCompatActivity {
+public class PageCourse extends Activity implements AdapterView.OnCreateContextMenuListener {
+
+    private Spinner spinner;
+    private static final String[]paths = {"item 1", "item 2", "item 3"};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_course);
+
+        spinner = (Spinner)findViewById(R.id.spinner1);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(PageCourse.this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+
     }
+
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+                // Whatever you want to happen when the first item gets selected
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                break;
+            case 2:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+
+        }
+    }
+
 }
