1156e5420c1f89449ab8e34eb3e96879cf9a3d0d
nawabhorn <38258798+nawabhorn@users.noreply.github.com>
2018-04-27 23:32:48 +0700

    Choose course

--- app/src/main/java/com/example/nonz/spaze/PageCourse.java
+++ app/src/main/java/com/example/nonz/spaze/PageCourse.java
@@ -1,9 +1,13 @@
 package com.example.nonz.spaze;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class PageCourse extends AppCompatActivity {
 
@@ -12,20 +16,62 @@ public class PageCourse extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_course);
 
-        Spinner mySpinner = (Spinner) findViewById(R.id.spinner1);
+        Spinner mySpinner1 = (Spinner) findViewById(R.id.spinner1);
 // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(PageCourse.this,
-                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
-        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mySpinner.setAdapter(myAdapter);
+        ArrayAdapter<String> myAdapter1 = new ArrayAdapter<String>(PageCourse.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names1));
+        myAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner1.setAdapter(myAdapter1);
 
-        /*
-        <Spinner
-                android:id="@+id/spinner1"
-                android:layout_width="match_parent"
-                android:layout_height="140dp"
-                android:background="@drawable/courseelementary"
-                android:spinnerMode="dropdown" />
-        */
+        Spinner mySpinner2 = (Spinner) findViewById(R.id.spinner1);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<String> myAdapter2 = new ArrayAdapter<String>(PageCourse.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names2));
+        myAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner2.setAdapter(myAdapter2);
+
+        mySpinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+
+                String s=((TextView)view).getText().toString();
+                if(s.equals("Level 1"))
+                    startActivity(new Intent(view.getContext(),PageLogin.class));
+                if(s.equals("Level 2"))
+                    startActivity(new Intent(view.getContext(),PageCourse.class));
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+
+        });
+
+        mySpinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+
+                String s=((TextView)view).getText().toString();
+                if(s.equals("Level 4"))
+                    startActivity(new Intent(view.getContext(),PageLogin.class));
+                if(s.equals("Level 5"))
+                    startActivity(new Intent(view.getContext(),PageCourse.class));
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+
+        });
     }
 }
