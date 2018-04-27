e30ed562be437c95ee2e6f8dac03a6cb11fdb21d
Supanut <supanut.kon@gmail.com>
2018-04-28 04:54:15 +0700

    Login, Signup and Databasehelper

--- app/src/main/java/com/example/nonz/spaze/PageLogin.java
+++ app/src/main/java/com/example/nonz/spaze/PageLogin.java
@@ -1,24 +1,44 @@
 package com.example.nonz.spaze;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class PageLogin  extends AppCompatActivity {
 
-    private EditText editTextUsername, editTextPassword;
-    private Button LoginBtn, RegisBtn;
+    DatabaseHelper helper = new DatabaseHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_login);
 
-        editTextUsername = findViewById(R.id.editTextUsername);
-        editTextPassword = findViewById(R.id.editTextPassword);
-        LoginBtn = findViewById(R.id.LoginButton);
+    }
+    public void onButtonClick(View v)
+    {
+        if(v.getId() == R.id.LoginButton)
+        {
+            EditText usr = (EditText)findViewById(R.id.editTextUsername);
+            EditText pwd = (EditText)findViewById(R.id.editTextPassword);
 
+            String usrstr = usr.getText().toString();
+            String pwdstr = pwd.getText().toString();
+
+            String password = helper.searchPass(usrstr);
+            if(pwdstr.equals(password))
+            {
+                Intent i = new Intent(PageLogin.this, MainActivity.class);
+
+            }
+            else
+            {
+                Toast temp = Toast.makeText(PageLogin.this, "Username and Password don't match.", Toast.LENGTH_SHORT);
+                temp.show();
+            }
+        }
     }
 }
