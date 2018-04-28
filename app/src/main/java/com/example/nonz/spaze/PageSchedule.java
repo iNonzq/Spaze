462389089257f106eb4f9ef84bef72bbfcd26ac6
nawabhorn <38258798+nawabhorn@users.noreply.github.com>
2018-04-28 14:52:56 +0700

    Calendar

--- app/src/main/java/com/example/nonz/spaze/PageSchedule.java
+++ app/src/main/java/com/example/nonz/spaze/PageSchedule.java
@@ -3,11 +3,22 @@ package com.example.nonz.spaze;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class PageSchedule extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_page_schedule);
+        setContentView(R.layout.activity_page_schedule);}
+
+        public long getLongAsDate(int year, int month, int date) {
+            Calendar calendar = new GregorianCalendar();
+            calendar.set(Calendar.DAY_OF_MONTH, date);
+            calendar.set(Calendar.MONTH, month - 1);
+            calendar.set(Calendar.YEAR, year);
+            return calendar.getTimeInMillis();
+        }
     }
-}
+
