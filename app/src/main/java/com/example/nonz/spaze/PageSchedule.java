9cce8062729bc9b9e2bc9817db160e2e394f065d
nawabhorn <38258798+nawabhorn@users.noreply.github.com>
2018-04-28 15:43:22 +0700

    SetEvent

--- app/src/main/java/com/example/nonz/spaze/PageSchedule.java
+++ app/src/main/java/com/example/nonz/spaze/PageSchedule.java
@@ -1,9 +1,15 @@
 package com.example.nonz.spaze;
 
+import android.content.Intent;
+import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.provider.CalendarContract.Events;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class PageSchedule extends AppCompatActivity {
@@ -19,6 +25,45 @@ public class PageSchedule extends AppCompatActivity {
             calendar.set(Calendar.MONTH, month - 1);
             calendar.set(Calendar.YEAR, year);
             return calendar.getTimeInMillis();
+
         }
+
+        public void onAddEventClicked(View view){
+            /*Intent intent = new Intent(Intent.ACTION_INSERT);
+            intent.setType("vnd.android.cursor.item/event");*/
+
+            Calendar cal = Calendar.getInstance();
+            long startTime = cal.getTimeInMillis();
+            long endTime = cal.getTimeInMillis()  + 60 * 60 * 1000;
+
+            String startDate = "2018-04-29";
+            try {
+                Date date = new SimpleDateFormat("yyyy-MM-dd").parse(startDate);
+                startTime=date.getTime();
+            }
+            catch(Exception e){ }
+
+            Calendar CalendarView = Calendar.getInstance();
+            Intent intent = new Intent(Intent.ACTION_EDIT);
+            intent.setType("vnd.android.cursor.item/event");
+            intent.putExtra("beginTime",startTime);
+            intent.putExtra("allDay", true);
+            intent.putExtra("rrule", "FREQ=YEARLY");
+            intent.putExtra("endTime", endTime);
+            intent.putExtra("title", "A Test Event from android app");
+            startActivity(intent);
+
+            /*intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, startTime);
+            intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,endTime);
+            intent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
+
+            intent.putExtra(CalendarContract.Events.TITLE, "Neel Birthday");
+            intent.putExtra(CalendarContract.Events.DESCRIPTION,  "This is a sample description");
+            intent.putExtra(CalendarContract.Events.EVENT_LOCATION, "My Guest House");
+            intent.putExtra(CalendarContract.Events.RRULE, "FREQ=YEARLY");
+
+        startActivity(intent);*/
+    }
+
     }
 
