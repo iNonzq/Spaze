5335b8fc1c6d9c25f2628d308cdc70a96b97c4fa
nawabhorn <38258798+nawabhorn@users.noreply.github.com>
2018-04-28 16:54:48 +0700

    Specific day

--- app/src/main/java/com/example/nonz/spaze/PageSchedule.java
+++ app/src/main/java/com/example/nonz/spaze/PageSchedule.java
@@ -1,69 +1,74 @@
 package com.example.nonz.spaze;
 
+import android.app.usage.UsageEvents;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.provider.CalendarContract;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.provider.CalendarContract.Events;
+import android.widget.Toast;
+
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 
 public class PageSchedule extends AppCompatActivity {
+
+    CompactCalendarView compactCalendar;
+    private SimpleDateFormat dateFormatMonth = new SimpleDateFormat("MMMM- yyyy", Locale.getDefault());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_page_schedule);}
+        setContentView(R.layout.activity_page_schedule);
 
-        public long getLongAsDate(int year, int month, int date) {
+
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setTitle(null);
+
+        compactCalendar = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
+        compactCalendar.setUseThreeLetterAbbreviation(true);
+
+        Event ev1 = new Event(Color.WHITE, 1524906000000L, "Test");
+        compactCalendar.addEvent(ev1);
+
+        compactCalendar.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                Context context = getApplicationContext();
+                    if(dateClicked.toString().compareTo("Sun 29 Apr 09:00:00 AST 2018")==0){
+                        Toast.makeText(context, "Test", Toast.LENGTH_SHORT).show();
+                    }
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                actionBar.setTitle(dateFormatMonth.format(firstDayOfNewMonth));
+            }
+        });
+
+    }
+
+        /*public long getLongAsDate(int year, int month, int date) {
             Calendar calendar = new GregorianCalendar();
             calendar.set(Calendar.DAY_OF_MONTH, date);
             calendar.set(Calendar.MONTH, month - 1);
             calendar.set(Calendar.YEAR, year);
             return calendar.getTimeInMillis();
 
-        }
+        }*/
 
-        public void onAddEventClicked(View view){
-            /*Intent intent = new Intent(Intent.ACTION_INSERT);
-            intent.setType("vnd.android.cursor.item/event");*/
 
-            Calendar cal = Calendar.getInstance();
-            long startTime = cal.getTimeInMillis();
-            long endTime = cal.getTimeInMillis()  + 60 * 60 * 1000;
-
-            String startDate = "2018-04-29";
-            try {
-                Date date = new SimpleDateFormat("yyyy-MM-dd").parse(startDate);
-                startTime=date.getTime();
-            }
-            catch(Exception e){ }
-
-            Calendar CalendarView = Calendar.getInstance();
-            Intent intent = new Intent(Intent.ACTION_EDIT);
-            intent.setType("vnd.android.cursor.item/event");
-            intent.putExtra("beginTime",startTime);
-            intent.putExtra("allDay", true);
-            intent.putExtra("rrule", "FREQ=YEARLY");
-            intent.putExtra("endTime", endTime);
-            intent.putExtra("title", "A Test Event from android app");
-            startActivity(intent);
-
-            /*intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, startTime);
-            intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,endTime);
-            intent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
-
-            intent.putExtra(CalendarContract.Events.TITLE, "Neel Birthday");
-            intent.putExtra(CalendarContract.Events.DESCRIPTION,  "This is a sample description");
-            intent.putExtra(CalendarContract.Events.EVENT_LOCATION, "My Guest House");
-            intent.putExtra(CalendarContract.Events.RRULE, "FREQ=YEARLY");*/
-
-        startActivity(intent);
-    }
 
     }
 
