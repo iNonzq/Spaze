c7e7a23d6626a8e46d6517613ff6f6a8148f4c13
nawabhorn <38258798+nawabhorn@users.noreply.github.com>
2018-04-29 11:26:51 +0700

    share

--- app/src/main/java/com/example/nonz/spaze/PageFact.java
+++ app/src/main/java/com/example/nonz/spaze/PageFact.java
@@ -1,15 +1,31 @@
 package com.example.nonz.spaze;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PageFact extends AppCompatActivity {
 
+    Button SButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_fact);
-
+        SButton = (Button)findViewById(R.id.SButton);
+        SButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(Intent.ACTION_SEND);
+                myIntent.setType("text/plain");
+                String shareBody = "Details Here";
+                String shareSub = "Name of Subject Here";
+                myIntent.putExtra(Intent.EXTRA_SUBJECT,shareSub);
+                myIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(myIntent,"Share using"));
+            }
+        });
 
     }
 }
