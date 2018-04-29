0c2b87be7229ca0a8ce59fa153597678c4895892
Nonz <Nonz@Apples-MacBook-Pro.local>
2018-04-29 17:41:46 +0700

    bug

--- app/src/main/java/com/example/nonz/spaze/PageSetting.java
+++ app/src/main/java/com/example/nonz/spaze/PageSetting.java
@@ -16,29 +16,5 @@ public class PageSetting extends AppCompatActivity {
 
     }
 }
-/*public class ProfileActivity extends AppCompatActivity {
 
-    LoginDataBaseAdapter loginDataBaseAdapter;
-//TextView profileName;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
-
-        loginDataBaseAdapter=new LoginDataBaseAdapter(this);
-        try {
-            loginDataBaseAdapter=loginDataBaseAdapter.open();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        TextView profileName = (TextView) findViewById(R.id.profileNameTextView);
-        String userName = loginDataBaseAdapter.getUserName("USERNAME");
-        String user = loginDataBaseAdapter.getUserName(userName);
-
-
-
-        profileName.setText("hello" + user);
-    }*/
 
